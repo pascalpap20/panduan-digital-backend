e8273b004f42aeb415bd66af84b51388c6732812
Pascal <pascalpanatagama@gmail.com>
2021-06-12 14:21:06 +0700

    cors

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,6 @@ use App\Http\Controllers\MataKuliahElektifController;
 |
 */
 
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Origin: *');
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
